291f126dc495b2ed3f1970f26848d826761f0cf3
MartinWrighton <martin.wrighton@outlook.com>
2024-02-19 17:22:52 +0000

    split new file tab down

--- gui.java
+++ gui.java
@@ -54,205 +54,211 @@ public class gui {
     }
 
     private void createNewFileTab(){
-        
-        //#region New File Tab
         JPanel panel1 = new JPanel();// new page panel
         this.panels.add(panel1);
+
         JTextField panel1_text1 = new JTextField(20);
         JButton panel1_button1 = new JButton("Create new File");// create tab button
 
-        //#region newly created tree tabs
-
-        ActionListener createNewTab = new ActionListener() {
+        panel1_button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JPanel newpanel = new JPanel();
-                panels.add(newpanel);
-                tabs.insertTab(panel1_text1.getText(), null, newpanel, null, tabs.getSelectedIndex());
-                //#region creating tree
-                node newNode = new node(panel1_text1.getText(), null,null,null);
-                DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(newNode);// configuring tree
-                newNode.setTreeNode(rootNode);
-                DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
-                newNode.setTreeModel(treeModel);
-                JTree tree = new JTree(treeModel);
-
-                //#region tree selection listener
-                tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-                tree.addTreeSelectionListener(new TreeSelectionListener() {
-                    @Override
-                    public void valueChanged(TreeSelectionEvent e) {
-                        DefaultMutableTreeNode clicked_node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-                        if (clicked_node == null) {
-                            return;
-                        }
-                        main_file.selected_node = (node) clicked_node.getUserObject();
-                        
-                        //System.out.println(main_file.selected_node);
-                        //System.out.println(main_file.selected_node.getTreeNode().getChildCount());
-
-                        //setting text on right panel THIS WORKS BY PIXEL PLACEMENT AND WIL BREAK IF YOU MOVE THE COMPONENTS
-                        JLabel current_page_title = (JLabel) main_file.gui.tabs.getSelectedComponent().getComponentAt(405,10).getComponentAt(10,0);
-                        current_page_title.setText(clicked_node.toString());
-                    }
-
-                });
-                //#endregion tree selection listener
-
-                //#region adding tree to storage and page
-                
-                
-                newpanel.setLayout(null);
-                JPanel newLeftPanel = new JPanel();
-                newpanel.add(newLeftPanel);
-                newLeftPanel.setBounds(10, 10, 385, 680);
-                
-                newLeftPanel.add(tree);
-                newLeftPanel.setLayout(null);
-                tree.setBounds(10, 10, 375, 670);
-                trees.add(tree);
-                //#endregion adding tree to storage and page
-            //#endregion creating tree
-
-                //#region right panel
-                //TODO add all different mutation types
-               
-                JPanel newRightPanel = new JPanel();//setting up right panel
-                newpanel.add(newRightPanel);
-                newRightPanel.setBounds(405, 10, 385, 600);
-                newRightPanel.setLayout(null);
-                newpanel.setBackground(new Color(200,200,255));
-
-                //adding titles to right panel
-                JLabel currentNodeLabel = new JLabel("-"); //MOVING THIS WILL PROVENT IT FROM WORKING (SEE TREE LISTENER) (this is a poor workaround)
-                currentNodeLabel.setBounds(10, 0, 300, 20);
-                currentNodeLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-                newRightPanel.add(currentNodeLabel);
-
-                JLabel rightPanelMutateLabel = new JLabel("Create New Children:");
-                rightPanelMutateLabel.setBounds(10, 15, 200, 40);
-                newRightPanel.add(rightPanelMutateLabel);
-
-                JLabel rightPanelTestLabel = new JLabel("Test this Node:");
-                rightPanelTestLabel.setBounds(10, 250, 200, 40);
-                newRightPanel.add(rightPanelTestLabel);
-
-                JButton goMutate = new JButton("Go (test)"); // setting up mutate button
-                goMutate.addActionListener( new ActionListener(){// process for creating a new child
-
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                    if(main_file.selected_node != null){
-                        //we will need a randomizer
-                        Random random = new Random();
-                        
-
-                        //source of event is the button
-                        JButton gButton = (JButton) e.getSource();
-                        //Right panel is the parent
-                        JPanel rPanel = (JPanel) gButton.getParent();
-                        //content of howManySpin while we are here
-                        int howManyNum = (int) ((JSpinner) rPanel.getComponentAt(200, 170)).getValue();
-                        //radio button is a child of parent
-                        if (((AbstractButton) rPanel.getComponentAt(10, 80)).isSelected()){//Pixel locator
-                            //pad numbers
-                            boolean isValid = ((AbstractButton) main_file.gui.tabs.getComponentAt(0).getComponentAt(10,10).getComponentAt(10,100)).isSelected();
-                            if (isValid == true){
-                                String frontPad = "";
-                                String backPad = "";
-                                for (int i = 0 ; i < howManyNum ; i++){
-                                    frontPad = frontPad +(Integer.toString(random.nextInt(10)));
-                                    backPad = backPad +(Integer.toString(random.nextInt(10)));
-                                }
-                                main_file.selected_node.addChild(frontPad+main_file.selected_node.getWord()+backPad);
-                            } else {//dialogue box
-                                JDialog dialog = new JDialog(main_file.gui.frame,"Your policy does not allow numbers!",true);
-                                dialog.setSize(300,20);
-                                dialog.setLocationRelativeTo(main_file.gui.frame);
-                                dialog.setVisible(true);
-                            }
-                        } else if (((AbstractButton) rPanel.getComponentAt(10, 110)).isSelected()){
-                            //homoglyph
-                        } else if (((AbstractButton) rPanel.getComponentAt(10, 140)).isSelected()){
-                            //capitalize
-                        } else {
-                            //else
-                            main_file.selected_node.addChild(new Date().toString());//date is the test name
-                        }
-                    }
-                    
-                }
-                });// this actionlistener creates children
-                goMutate.setBackground(new Color(255,105,97));
-                goMutate.setBounds(200, 200, 150, 20);
-                goMutate.setToolTipText("This is a test button to create test children");
-                newRightPanel.add(goMutate);
-
-                    //#region example mutate options
-                JRadioButton mutateRadioButtonT1 = new JRadioButton("Pad Numbers");
-                mutateRadioButtonT1.setBounds(10, 80, 150, 20);
-                mutateRadioButtonT1.setToolTipText("Create a child by padding the parent with numbers");
-                newRightPanel.add(mutateRadioButtonT1);
-                JRadioButton mutateRadioButtonT2 = new JRadioButton("Homoglyph");
-                mutateRadioButtonT2.setBounds(10, 110, 150, 20);
-                mutateRadioButtonT2.setToolTipText("Create a child by exchanging characters with homoglyphs");
-                newRightPanel.add(mutateRadioButtonT2);
-
-                JRadioButton mutateRadioButtonT3 = new JRadioButton("Capitalize");
-                mutateRadioButtonT3.setBounds(10, 140, 150, 20);
-                mutateRadioButtonT3.setToolTipText("Create a child by capitalizing random characters");
-                newRightPanel.add(mutateRadioButtonT3);
-
-                ButtonGroup mutateRadioGroup = new ButtonGroup();
-                mutateRadioGroup.add(mutateRadioButtonT1);
-                mutateRadioGroup.add(mutateRadioButtonT2);
-                mutateRadioGroup.add(mutateRadioButtonT3);
-                    //#endregion
-
-                    //#region how many X
-                JLabel howManyLabel = new JLabel("How many?");
-                howManyLabel.setBounds(200, 150, 200, 20);
-                newRightPanel.add(howManyLabel);
-
-                
-                JSpinner howManySpin = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
-                howManySpin.setBounds(200, 170, 40, 20);
-                newRightPanel.add(howManySpin);
-                    //#endregion
-                
-                    //#region test password
-                    JButton goTest = new JButton("Test");
-                    goTest.setBounds(10, 300, 80, 20);
-                    newRightPanel.add(goTest);
-                    goTest.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e){
-                            passwordCheck checker = new passwordCheck(main_file.selected_node.getWord(),"martin");
-                            //TODO add file handling, maybe just concat the whole file (make sure to remove endlines ) and patternmatch against the whole thing?
-                            checker.execute();
-                        }});
-                    //#endregion test password
-                
-
-                //#endregion right panel
-
-            }
-        };
-        //#endregion Process for creating a new file tab
-        
-        panel1_button1.addActionListener(createNewTab);// this actionlistener creates new tabs
+                createNewWorkingFileTab(panel1_text1.getText());
+            }});
         panel1.add(panel1_button1);
         panel1.add(panel1_text1);
+
         this.tabs.insertTab("New+", null, panel1, "Create a new file", 0);
         this.tabs.setBounds(0, 0, 800, 600);
-        //#endregion New File Tab
+
+
+       
    
     }
 
 
+    private void createNewWorkingFileTab(String rootPassword){
+        //create new panel
+        JPanel newpanel = new JPanel();
+        newpanel.setBackground(new Color(200,200,255));
+        newpanel.setLayout(null);
+        panels.add(newpanel);
+        tabs.insertTab(rootPassword, null, newpanel, null, tabs.getSelectedIndex());
+
+        
+
+        createLeftPanel(newpanel,rootPassword);
+        
+        createRightPanel(newpanel);
+
+    }
+        
+    private void createLeftPanel(JPanel newpanel,String rootPassword){
+        //create left panel
+        JPanel newLeftPanel = new JPanel();
+        newpanel.add(newLeftPanel);
+        newLeftPanel.setBounds(10, 10, 385, 680);
+        newLeftPanel.setLayout(null);
+
+        //create tree and place in left panel
+        JTree tree = createNewTree(rootPassword);
+        tree.setBounds(10, 10, 375, 670);
+        newLeftPanel.add(tree);
+        trees.add(tree);
+    }
+
+    private void createRightPanel(JPanel newpanel){
+        //TODO add all different mutation types
+        
+        JPanel newRightPanel = new JPanel();//setting up right panel
+        newpanel.add(newRightPanel);
+        newRightPanel.setBounds(405, 10, 385, 600);
+        newRightPanel.setLayout(null);
+        
+
+        //adding titles to right panel
+        JLabel currentNodeLabel = new JLabel("-"); //MOVING THIS WILL PROVENT IT FROM WORKING (SEE TREE LISTENER) (this is a poor workaround)
+        currentNodeLabel.setBounds(10, 0, 300, 20);
+        currentNodeLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        newRightPanel.add(currentNodeLabel);
+
+        //labels
+        JLabel rightPanelMutateLabel = new JLabel("Create New Children:");
+        rightPanelMutateLabel.setBounds(10, 15, 200, 40);
+        newRightPanel.add(rightPanelMutateLabel);
+
+        JLabel rightPanelTestLabel = new JLabel("Test this Node:");
+        rightPanelTestLabel.setBounds(10, 250, 200, 40);
+        newRightPanel.add(rightPanelTestLabel);
+
+        JButton goMutate = new JButton("Go (test)"); // setting up mutate button
+        goMutate.setBackground(new Color(255,105,97));
+        goMutate.setBounds(200, 200, 150, 20);
+        goMutate.setToolTipText("This is a test button to create test children");
+        newRightPanel.add(goMutate);
+        goMutate.addActionListener( new ActionListener(){// process for creating a new child
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createChildNode(e);
+            }});// this actionlistener creates children
 
 
+        JRadioButton mutateRadioButtonT1 = new JRadioButton("Pad Numbers");
+        mutateRadioButtonT1.setBounds(10, 80, 150, 20);
+        mutateRadioButtonT1.setToolTipText("Create a child by padding the parent with numbers");
+        newRightPanel.add(mutateRadioButtonT1);
+        JRadioButton mutateRadioButtonT2 = new JRadioButton("Homoglyph");
+        mutateRadioButtonT2.setBounds(10, 110, 150, 20);
+        mutateRadioButtonT2.setToolTipText("Create a child by exchanging characters with homoglyphs");
+        newRightPanel.add(mutateRadioButtonT2);
+
+        JRadioButton mutateRadioButtonT3 = new JRadioButton("Capitalize");
+        mutateRadioButtonT3.setBounds(10, 140, 150, 20);
+        mutateRadioButtonT3.setToolTipText("Create a child by capitalizing random characters");
+        newRightPanel.add(mutateRadioButtonT3);
+
+        ButtonGroup mutateRadioGroup = new ButtonGroup();
+        mutateRadioGroup.add(mutateRadioButtonT1);
+        mutateRadioGroup.add(mutateRadioButtonT2);
+        mutateRadioGroup.add(mutateRadioButtonT3);
+
+        JLabel howManyLabel = new JLabel("How many?");
+        howManyLabel.setBounds(200, 150, 200, 20);
+        newRightPanel.add(howManyLabel);
+
+        
+        JSpinner howManySpin = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
+        howManySpin.setBounds(200, 170, 40, 20);
+        newRightPanel.add(howManySpin);
+
+        JButton goTest = new JButton("Test");
+        goTest.setBounds(10, 300, 80, 20);
+        newRightPanel.add(goTest);
+        goTest.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                passwordCheck checker = new passwordCheck(main_file.selected_node.getWord(),"martin");
+                //TODO add file handling, maybe just concat the whole file (make sure to remove endlines ) and patternmatch against the whole thing?
+                //that might need a function of its own
+                checker.execute();
+            }});
+            
+
+    }
+
+    private void createChildNode(ActionEvent e){
+        if(main_file.selected_node != null){
+            //we will need a randomizer
+            Random random = new Random();
+            
+
+            //source of event is the button
+            JButton gButton = (JButton) e.getSource();
+            //Right panel is the parent
+            JPanel rPanel = (JPanel) gButton.getParent();
+            //content of howManySpin while we are here
+            int howManyNum = (int) ((JSpinner) rPanel.getComponentAt(200, 170)).getValue();
+            //radio button is a child of parent
+            if (((AbstractButton) rPanel.getComponentAt(10, 80)).isSelected()){//Pixel locator
+                //pad numbers
+                boolean isValid = ((AbstractButton) main_file.gui.tabs.getComponentAt(0).getComponentAt(10,10).getComponentAt(10,100)).isSelected();
+                if (isValid == true){
+                    String frontPad = "";
+                    String backPad = "";
+                    for (int i = 0 ; i < howManyNum ; i++){
+                        frontPad = frontPad +(Integer.toString(random.nextInt(10)));
+                        backPad = backPad +(Integer.toString(random.nextInt(10)));
+                    }
+                    main_file.selected_node.addChild(frontPad+main_file.selected_node.getWord()+backPad);
+                } else {//dialogue box
+                    JDialog dialog = new JDialog(main_file.gui.frame,"Your policy does not allow numbers!",true);
+                    dialog.setSize(300,20);
+                    dialog.setLocationRelativeTo(main_file.gui.frame);
+                    dialog.setVisible(true);
+                }
+            } else if (((AbstractButton) rPanel.getComponentAt(10, 110)).isSelected()){
+                //homoglyph
+            } else if (((AbstractButton) rPanel.getComponentAt(10, 140)).isSelected()){
+                //capitalize
+            } else {
+                //else
+                main_file.selected_node.addChild(new Date().toString());//date is the test name
+            }
+        }
+        
+    }
     
+
+    private JTree createNewTree(String rootPassword){
+         node newNode = new node(rootPassword, null,null,null);
+         DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(newNode);// configuring tree and combining with our node class
+         newNode.setTreeNode(rootNode);
+         DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
+         newNode.setTreeModel(treeModel);
+         JTree tree = new JTree(treeModel);
+         tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+         tree.addTreeSelectionListener(new TreeSelectionListener() {//when node is clicked
+             @Override
+             public void valueChanged(TreeSelectionEvent e) {
+                 DefaultMutableTreeNode clicked_node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();//get the clicked node
+                 if (clicked_node == null) {
+                     return;
+                 }
+                 main_file.selected_node = (node) clicked_node.getUserObject();//save clicked node (as a node object)
+
+                 //setting text on right panel THIS WORKS BY PIXEL PLACEMENT AND WIL BREAK IF YOU MOVE THE COMPONENTS
+                 JLabel current_page_title = (JLabel) main_file.gui.tabs.getSelectedComponent().getComponentAt(405,10).getComponentAt(10,0);
+                 current_page_title.setText(clicked_node.toString());
+             }
+ 
+         });
+
+         return tree;
+         
+    }
+
     private void createSettingsTab(){
         JPanel settings = new JPanel();
         settings.setBackground(new Color(200,200,255));
